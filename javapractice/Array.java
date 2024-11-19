b3b473e0946f8ed25ed91ee084301b3b8ff66195
piyanshu31 <piyanshukale2005@gmail.com>
2024-11-19 22:28:55 +0530

    Update

--- javapractice/Array.java
+++ javapractice/Array.java
@@ -1,4 +1,20 @@
-package javapractice;
-public class Array {
-    
+class Array {
+	public static void main(String args[])
+	{
+		int a[]= {1,4,6,7,9,12,16};
+		int i;
+		System.out.print("Element of Array: ");
+		for(i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		System.out.println();
+		float sum=0,avg;
+		for(i=0;i<a.length;i++)
+		{
+			sum=sum+a[i];
+		}
+		avg=sum/a.length;
+		System.out.println("Average ="+avg);
+	}
 }
